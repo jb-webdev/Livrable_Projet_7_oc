97e6c3ee807029b7ec446f36f8ba95555855e714
j.boero <boero.dw@gmail.com>
2020-10-04 13:59:26 +0200

    commentaire

--- backend/config/dbconnect.js
+++ backend/config/dbconnect.js
@@ -1,10 +1,12 @@
 const mysql = require("mysql");
 const express = require('express');
-
+// rajouter un mots de passe robuste  à la ligne 8
+// pour sécuriser la connection à la base de données très très important 
+// préconiser une injection de variable
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
+  password: "", 
   database: "groupomaniadb"
 });
 
